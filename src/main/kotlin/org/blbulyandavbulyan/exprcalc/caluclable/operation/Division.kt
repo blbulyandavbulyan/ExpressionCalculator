0aa12b7117aff40f6ec9b1758d3dd5e14154d998
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-10-01 20:01:03 +0400

    Now all binary operations marked with Operator annotation

--- src/main/kotlin/org/blbulyandavbulyan/exprcalc/caluclable/operation/Division.kt
+++ src/main/kotlin/org/blbulyandavbulyan/exprcalc/caluclable/operation/Division.kt
@@ -1,7 +1,8 @@
 package org.blbulyandavbulyan.exprcalc.caluclable.operation
 
+import org.blbulyandavbulyan.exprcalc.annotations.Operator
 import org.blbulyandavbulyan.exprcalc.caluclable.Calculable
-
+@Operator("/")
 class Division(a: Calculable, b: Calculable) : BinaryOperation(a, b) {
     override fun calc(): Double = a.calc() / b.calc()
 }
--- src/main/kotlin/org/blbulyandavbulyan/exprcalc/caluclable/operation/Power.kt
+++ src/main/kotlin/org/blbulyandavbulyan/exprcalc/caluclable/operation/Power.kt
@@ -1,8 +1,9 @@
 package org.blbulyandavbulyan.exprcalc.caluclable.operation
 
+import org.blbulyandavbulyan.exprcalc.annotations.Operator
 import org.blbulyandavbulyan.exprcalc.caluclable.Calculable
 import kotlin.math.pow
-
+@Operator("^")
 class Power(a: Calculable, b: Calculable) : BinaryOperation(a, b) {
     override fun calc(): Double = a.calc().pow(b.calc())
 }
