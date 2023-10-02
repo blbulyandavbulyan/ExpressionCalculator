1257088a4365ac3690f3f1f489d1002586f9f314
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-10-02 14:54:29 +0400

    added OperationFactory#getOperatorsInfos and OperationFactory#getFunctionNames

--- src/main/kotlin/org/blbulyandavbulyan/exprcalc/rpn/OperationFactory.kt
+++ src/main/kotlin/org/blbulyandavbulyan/exprcalc/rpn/OperationFactory.kt
@@ -1,22 +1,26 @@
 package org.blbulyandavbulyan.exprcalc.rpn
 
-import org.blbulyandavbulyan.exprcalc.annotations.Operator
+import org.blbulyandavbulyan.exprcalc.annotations.Name
 import org.blbulyandavbulyan.exprcalc.caluclable.Calculable
 import org.blbulyandavbulyan.exprcalc.caluclable.operation.Operation
 import org.reflections.Reflections
 import java.lang.reflect.Constructor
 import java.lang.reflect.Modifier
-
 class OperationFactory {
-    private val operators: MutableMap<String, Constructor<out Operation>> = HashMap()
+
+    private val operations: MutableMap<String, Constructor<out Operation>> = HashMap()
     init {
         getAllOperationSubClasses()
-            .filter { !Modifier.isAbstract(it.modifiers) && it.isAnnotationPresent(Operator::class.java)}
-            .map { Pair(it.getAnnotation(Operator::class.java).name, it.constructors[0] as Constructor<out Operation>) }
-            .map { it }.toMap(operators)
+            .filter { !Modifier.isAbstract(it.modifiers) && it.isAnnotationPresent(Name::class.java) }
+            .associateTo(operations) {
+                Pair(
+                    it.getAnnotation(Name::class.java).name,
+                    it.constructors[0] as Constructor<out Operation>
+                )
+            }
     }
     fun create(operatorName: String, getParameter: ()->Calculable): Operation{
-        val operationProducer = operators[operatorName] ?: throw UnsupportedOperationException("Operator $operatorName is not supported!")
+        val operationProducer = operations[operatorName] ?: throw UnsupportedOperationException("Operator $operatorName is not supported!")
         val size = operationProducer.parameters.size
         val args: MutableList<Calculable> = ArrayList(size)
         repeat(size){
@@ -28,5 +32,12 @@ class OperationFactory {
         val reflections = Reflections("org.blbulyandavbulyan.exprcalc.caluclable.operation");
         return reflections.getSubTypesOf(Operation::class.java)
     }
-    operator fun contains(operatorName: String): Boolean = operatorName in operators
+    operator fun contains(operatorName: String): Boolean = operatorName in operations
+    fun getOperatorsInfos(): Map<String, OperatorInfo> = operations.entries
+        .map { Pair(it.key, it.value.getAnnotation(org.blbulyandavbulyan.exprcalc.annotations.OperatorInfo::class.java)) }
+        .filter { it.second != null }
+        .associate { Pair(it.first, OperatorInfo(it.second.priority, it.second.associativity)) }
+    fun getFunctionNames(): Collection<String> =
+        operations.entries.filter { Operation.AbstractFunction::class.java.isAssignableFrom(it.value.declaringClass)}
+            .map { it.key }
 }
