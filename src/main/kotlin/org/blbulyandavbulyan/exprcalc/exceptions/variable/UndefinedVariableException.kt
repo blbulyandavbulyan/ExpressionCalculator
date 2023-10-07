f9bcc4dc27edc2d08b829d78ccff7a7dbd1812f7
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-10-07 16:33:44 +0400

    VariableNotInitializedException and UndefinedVariableException now extend ExpressionCalculatorException

--- src/main/kotlin/org/blbulyandavbulyan/exprcalc/exceptions/variable/UndefinedVariableException.kt
+++ src/main/kotlin/org/blbulyandavbulyan/exprcalc/exceptions/variable/UndefinedVariableException.kt
@@ -1,5 +1,5 @@
 package org.blbulyandavbulyan.exprcalc.exceptions.variable
 
-import java.lang.RuntimeException
+import org.blbulyandavbulyan.exprcalc.exceptions.ExpressionCalculatorException
 
-class UndefinedVariableException(variableName: String) : RuntimeException("Variable '$variableName' is undefined!")
+class UndefinedVariableException(variableName: String) : ExpressionCalculatorException("Variable '$variableName' is undefined!")
--- src/main/kotlin/org/blbulyandavbulyan/exprcalc/exceptions/variable/VariableNotInitializedException.kt
+++ src/main/kotlin/org/blbulyandavbulyan/exprcalc/exceptions/variable/VariableNotInitializedException.kt
@@ -1,6 +1,6 @@
 package org.blbulyandavbulyan.exprcalc.exceptions.variable
 
-import java.lang.RuntimeException
+import org.blbulyandavbulyan.exprcalc.exceptions.ExpressionCalculatorException
 
-class VariableNotInitializedException(variableName: String): RuntimeException("Variable $variableName is not initialized!") {
+class VariableNotInitializedException(variableName: String): ExpressionCalculatorException("Variable $variableName is not initialized!") {
 }
