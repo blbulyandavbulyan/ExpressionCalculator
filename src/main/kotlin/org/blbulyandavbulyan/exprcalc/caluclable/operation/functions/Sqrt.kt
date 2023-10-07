3c798a66da09a7db3f04e123a94c34be6699f02c
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-10-07 22:00:34 +0400

    operator.functions package was renamed to operator.functions

--- src/main/kotlin/org/blbulyandavbulyan/exprcalc/caluclable/operation/functions/Sqrt.kt
+++ src/main/kotlin/org/blbulyandavbulyan/exprcalc/caluclable/operation/functions/Sqrt.kt
@@ -1,4 +1,4 @@
-package org.blbulyandavbulyan.exprcalc.caluclable.operation.function
+package org.blbulyandavbulyan.exprcalc.caluclable.operation.functions
 
 import org.blbulyandavbulyan.exprcalc.annotations.Name
 import org.blbulyandavbulyan.exprcalc.caluclable.Calculable
