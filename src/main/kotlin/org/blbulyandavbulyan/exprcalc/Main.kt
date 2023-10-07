d7eea51aa77308952d3c81cf43d3a1f5ab058d7e
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-10-07 17:26:35 +0400

    added exception handling in Main

--- src/main/kotlin/org/blbulyandavbulyan/exprcalc/Main.kt
+++ src/main/kotlin/org/blbulyandavbulyan/exprcalc/Main.kt
@@ -1,23 +1,29 @@
 package org.blbulyandavbulyan.exprcalc
 
-import org.blbulyandavbulyan.exprcalc.rpn.OperationFactory
-import org.blbulyandavbulyan.exprcalc.rpn.RPNConverter
+import org.blbulyandavbulyan.exprcalc.exceptions.ExpressionCalculatorException
 import org.blbulyandavbulyan.exprcalc.rpn.ExpressionFactory
+import org.blbulyandavbulyan.exprcalc.rpn.OperationFactory
 
 fun main() {
     val expressionFactory = ExpressionFactory(OperationFactory())
-    var command: String = readln()
-    while (command != "exit"){
-        val expression = expressionFactory.parseExpression(command);
-        val variableNames = expression.variableNames
-        if(variableNames.isNotEmpty()){
-            //здесь должен быть цикл запроса значений переменных
-            for (variableName in variableNames) {
-                print("Введите значение переменной $variableName: ")
-                expression[variableName] = readln().toDouble()
-            }
-        }
-        println(expression.calc())
+    var command: String;
+    while (true){
         command = readln()
+        if(command == "exit")break;
+        try {
+            val expression = expressionFactory.parseExpression(command);
+            val variableNames = expression.variableNames
+            if(variableNames.isNotEmpty()){
+                //здесь должен быть цикл запроса значений переменных
+                for (variableName in variableNames) {
+                    print("Введите значение переменной $variableName: ")
+                    expression[variableName] = readln().toDouble()
+                }
+            }
+            println(expression.calc())
+        }
+        catch (e: ExpressionCalculatorException){
+            println(e.message)
+        }
     }
 }
