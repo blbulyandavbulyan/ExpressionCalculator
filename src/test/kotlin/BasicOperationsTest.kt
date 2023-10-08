96a8c72ae93cb0248b22a6bcd3a81e291c380586
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-10-08 19:02:55 +0400

    added additional tests

--- src/test/kotlin/BasicOperationsTest.kt
+++ src/test/kotlin/BasicOperationsTest.kt
@@ -2,6 +2,7 @@
 import org.blbulyandavbulyan.exprcalc.rpn.ExpressionParserBuilder
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.math.pow
 
 
 private class BasicOperationsTest {
@@ -45,7 +46,64 @@ private class BasicOperationsTest {
         expression["d"] = 3.0
         assertEquals(729.0, expression.calc(), 0.0001)
     }
+    @Test
+    fun testComplexExpression1() {
+        val expression = expressionParser("(a + b) * (c - d)")
+        val a = 5.0
+        val b = 3.0
+        val c = 10.0
+        val d = 2.0
+        expression["a"] = a
+        expression["b"] = b
+        expression["c"] = c
+        expression["d"] = d
+        assertEquals((a + b) * (c - d), expression.calc(), 0.0001)
+    }
 
+    @Test
+    fun testComplexExpression2() {
+        val expression = expressionParser("(a * b) + ((c / d) - (e ^ f))")
+        val a = 2.0
+        val b = 3.0
+        val c = 15.0
+        val d = 5.0
+        val e = 2.0
+        val f = 3.0
+        expression["a"] = a
+        expression["b"] = b
+        expression["c"] = c
+        expression["d"] = d
+        expression["e"] = e
+        expression["f"] = f
+        assertEquals((a * b) + ((c / d) - (e.pow(f))), expression.calc(), 0.0001)
+    }
+
+    @Test
+    fun testComplexExpression3() {
+        val expression = expressionParser("((a + b) * c) / (d ^ e)")
+        val a = 1.0
+        val b = 2.0
+        val c = 6.0
+        val d = 3.0
+        val e = 2.0
+        expression["a"] = a
+        expression["b"] = b
+        expression["c"] = c
+        expression["d"] = d
+        expression["e"] = e
+        assertEquals(((a + b) * c) / (d.pow(e)), expression.calc(), 0.0001)
+    }
+
+    @Test
+    fun testNestedParentheses() {
+        val expression = expressionParser("((a + (b * c)) - (d ^ e))")
+        expression["a"] = 1.0
+        expression["b"] = 2.0
+        expression["c"] = 3.0
+        expression["d"] = 2.0
+        expression["e"] = 3.0
+        assertEquals(-1.0, expression.calc(), 0.0001)
+    }
     @Test
     fun testComplexExpressionWithVariableAssignmentAndUsage() {
         assertEquals(6.0, expressionParser("2 * 3").calc(), 0.0001)
